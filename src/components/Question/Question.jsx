7ca45200f788b4536c5cbd7c72d4fa214058dcd6
Lautaro Rocha <lautarovrocha@gmail.com>
2022-12-12 14:32:49 -0300

    getting the question from the db

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import useFetch from '../../useFetch';
 import "./question.css"
 import { useParams } from 'react-router-dom';
 
 const Question = () => {
+  const [question, setQuestion] = useState()
+  const { questionId } = useParams();
 
-    const { questionId } = useParams();
+  const questionUrl= `http://localhost:7000/ask/${questionId}`
+  
+  useEffect(()=>{
+    fetch(questionUrl)
+    .then(res => res.json())
+    .then(data => setQuestion(data))
+  }, [])
 
-    {console.log(questionId)}
+
+
     return (
         <>
         <main>
