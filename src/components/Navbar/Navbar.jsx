ccee421762dd135c061b73435403dd6fde88d001
Lautaro Rocha <lautarovrocha@gmail.com>
2022-12-20 17:06:59 -0300

    fixed behaviour of search bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
-import React, {useRef, forwardRef} from 'react';
+import React, {useRef, forwardRef, useEffect} from 'react';
 import './navbar.css'
 import * as Icons from "../../utilities/svgIcons"
 import Searchbar from '../SearchBar/Searchbar';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 const Navbar = forwardRef((props, ref) => {
 
     const navigate = useNavigate()
     const search = useRef()
+    const location = useLocation()
 
     function showOrHideSearchBar(){
-        search.current.style.opacity == 0 ?  search.current.style.opacity = 1 :  search.current.style.opacity = 0;
+        if(location.pathname === '/'){
+        search.current.style.opacity == 0 ?  search.current.style.opacity = 1 :  search.current.style.opacity = 0;}
     }
     function goHome(){
         navigate('/')
@@ -19,6 +21,9 @@ const Navbar = forwardRef((props, ref) => {
     function goProfile(){
         navigate('/user')
     }
+    useEffect(()=>{
+        search.current.style.opacity = 0
+    }, [location])
 
     return (
         <>
