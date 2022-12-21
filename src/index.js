f005fbacda764cdc1347cc402992bbd599875ed4
Lautaro Rocha <lautarovrocha@gmail.com>
2022-12-21 13:02:14 -0300

    firebase init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQUrC9tUGpei6Zrbm7NNFlJtgImAUBi_c",
+  authDomain: "dptalk-pics.firebaseapp.com",
+  projectId: "dptalk-pics",
+  storageBucket: "dptalk-pics.appspot.com",
+  messagingSenderId: "358298343111",
+  appId: "1:358298343111:web:3be0e345c8913f9be3d0e2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
