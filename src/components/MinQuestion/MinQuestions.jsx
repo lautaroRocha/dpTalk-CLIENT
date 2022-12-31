9b408d655882fa1ca06f79d1508f937463c36343
Lautaro Rocha <lautarovrocha@gmail.com>
2022-12-31 16:46:45 -0300

    added spinner loader to minquestion img

--- src/components/MinQuestion/MinQuestions.jsx
+++ src/components/MinQuestion/MinQuestions.jsx
@@ -1,28 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
+import Spinner from '../Spinner/Spinner'
 import "./minquestion.css"
 import getProfilePicture from '../../utilities/getPPURL';
 
 
 const MinQuestions = (props) => {
 
-    const [profilePictureUrl, setProfilePicUrl] = useState(null)
+    const [profilePictureUrl, setProfilePicUrl] = useState()
 
     const cachedURL = sessionStorage.getItem(`ProPic-${props.question._id}`)
 
-    if(cachedURL && !profilePictureUrl){
-        setProfilePicUrl(cachedURL)
-    }else{
-        getProfilePicture(props.question.author, setProfilePicUrl)
-        sessionStorage.setItem(`ProPic-${props.question._id}`, profilePictureUrl )
-    }
+    useEffect(()=>{
+        if(cachedURL && !profilePictureUrl){
+            setProfilePicUrl(cachedURL)
+        }else{
+            getProfilePicture(props.question.author, setProfilePicUrl)
+            sessionStorage.setItem(`ProPic-${props.question._id}`, profilePictureUrl )
+        }
+    },[])
+   
     
     
 
     return (
         <Link className='min-question'  to={`question/${props.question._id}`} >
             <div className='min-question-author'>
+                {!profilePictureUrl ?
+                <Spinner className='min-question-author-img' /> : 
                 <img className='min-question-author-img' src={`${profilePictureUrl}` }/>
+                }
                 <span className='min-queston-author-name'>{props.question.author}</span>
             </div>
                 <span className='min-question-status'>{!props.question.status ? <></> : "CONTESTADA"}</span>
