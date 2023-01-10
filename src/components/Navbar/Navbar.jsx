1181b2ffc762d5eb9fa8c9d456f6d27d767659fd
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-10 15:42:47 -0300

    added a confirm modal and library for future uses

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import {  useLocation, NavLink } from 'react-router-dom';
 import UserContext from '../../Context/UserContext';
 import { Tooltip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
-
+import Swal from 'sweetalert2'
 
 
 export const Navbar = forwardRef((props, ref) => {
@@ -25,6 +25,32 @@ export const Navbar = forwardRef((props, ref) => {
     }
     }
 
+    function askIfSure(){
+      Swal.fire({
+        title: 'Estás por cerrar sesión',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Cerrar sesión',
+        customClass: {
+          popup: 'swal-cont',
+          icon: 'swal-icon',
+          image: 'your-image-class',
+          content: 'your-content-class',
+          input: 'your-input-class',
+          actions: 'swal-action',
+          confirmButton: 'your-confirm-button-class',
+          cancelButton: 'your-cancel-button-class',
+          footer: 'your-footer-class'
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          props.logOut()
+        }
+      })
+    }
 
     useEffect(()=>{
         search.current.style.opacity = 0
@@ -48,7 +74,7 @@ export const Navbar = forwardRef((props, ref) => {
                 </li>
                 <li onClick={showOrHideSearchBar} id="Buscar"><NavLink>{Icons.search}</NavLink>
                 </li>
-                <li onClick={props.logOut} id="Salir"><NavLink>{Icons.logOut}</NavLink>
+                <li onClick={askIfSure} id="Salir"><NavLink>{Icons.logOut}</NavLink>
                 </li>
                 <>
                   <Tooltip className="tooltip" anchorId="Feed" content="Feed" place="right" variant='info'/>
